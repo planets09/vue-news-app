4bf4df5acfc2863ecafbafa9f9718ba083ec25d9
Rena Tan <rena.tanrx@gmail.com>
2019-02-14 22:54:14 -0800

    added news api url

--- src/App.vue
+++ src/App.vue
@@ -24,22 +24,29 @@ export default {
     return {
       articles: [
         {
-         title: "Article 1: Morbi non pretium purus",
+         title: "Article 1",
          description: "Pellentesque id ex in ligula vestibulum dictum at et lorem. Nam quis ultricies purus, ac interdum sem. Nam porta quis leo id ullamcorper." 
         },
         {
-         title: "Article 2: Vestibulum tempus",
+         title: "Article 2",
          description: "Cras dictum varius diam, at finibus leo varius ac. Aenean et libero" 
         },
         {
-         title: "Article 3: Curabitur at odio ligula",
+         title: "Article 3",
          description: "Etiam sagittis sed diam ac egestas. Ut viverra" 
         },
       ]
     }
   },
-  methods: {
-
+  mounted: function(){
+    var url = 'https://newsapi.org/v2/top-headlines?' +
+          'country=us&' +
+          'apiKey=57e7cd5efcd041d8977efdfa1d1e4059';
+var req = new Request(url);
+fetch(req)
+    .then(function(response) {
+        console.log(response.json());
+    })
   },
   components: {
     // appHello: HelloWorld
